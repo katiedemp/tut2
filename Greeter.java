e8a6de6f1c88afbe5962e7084109b3b0df2d291a
Katie <katie.m.dempsey@gmail.com>
2018-08-09 13:34:25 +1200

    Changed Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -15,15 +15,4 @@ public class Greeter {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
     return messages.getString("greeting") + ", " + "_"+name.toUpperCase()+"_";
   }
-
-  public static void main(String[] args) {
-    if (args.length < 3) {
-      System.exit(1);
-    }
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
-  }
 }
