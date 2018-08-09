539e983a69491dbd835d58ef0e9da130eedacb4b
Katie <katie.m.dempsey@gmail.com>
2018-08-09 12:11:14 +1200

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + "_"+name+"_";
+    return messages.getString("greeting") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
